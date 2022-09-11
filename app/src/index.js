e6f90888a6b65ca8ee09fb0715c3efc51fbd40b9
sadie100 <sadie1001@naver.com>
2022-09-11 16:27:07 +0900

    feat : "styled-component install, themeprovider 생성"

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
         <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
