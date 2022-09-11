44229f110938df971003ceebf0cfefdcafad139c
sadie100 <sadie1001@naver.com>
2022-09-11 17:04:23 +0900

    feat : "레이아웃 헤더 구조 잡음"

--- app/src/pages/App.js
+++ app/src/pages/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import Login from "./Login";
@@ -6,15 +7,37 @@ import Join from "./Join";
 
 const App = () => {
   return (
-    <div className="App">
+    <Background>
+      <Header>
+        <div>TimeFit.</div>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <div>로그인</div>
+          <div>|</div>
+          <div>회원가입</div>
+        </div>
+      </Header>
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/join" element={<Join />}></Route>
 
         <Route path="/login" element={<Login />}></Route>
       </Routes>
-    </div>
+    </Background>
   );
 };
+
+const Background = styled.div`
+  height: 100%;
+  width: 100vw;
+  background-color: white;
+`;
+const Header = styled.div`
+  height: 50px;
+  width: 100%;
+  background-color: ${(props) => props.theme.main.color};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export default App;
