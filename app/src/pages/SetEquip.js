b9183c0d97639398372c525a0aa2d28bc674c855
김세희 <82367039+sadie100@users.noreply.github.com>
2022-10-01 23:42:10 +0900

    fix : 운동기구 추가 페이지 구현 중

--- app/src/pages/SetEquip.js
+++ app/src/pages/SetEquip.js
@@ -4,10 +4,22 @@ import axios from "axios";
 const SetEquip = () => {
   const onSubmit = async (data) => {
     try {
-      const { status } = await axios.post("/equipment/add", data);
-      if (status === 200) {
-        alert("저장되었습니다.");
+      const respond = await axios.post("/equipment/add", data);
+      if (respond.status !== 200) {
+        return alert("오류가 일어났습니다. 다시 시도해 주세요.");
       }
+      console.log(respond);
+      const id = respond.data.id;
+
+      //센터 이미지 추가
+      let formData = new FormData();
+      formData.append("equipmentId", 3);
+      formData.append("file", data.file);
+      console.log("eid", formData.get("equipmentId"));
+      console.log("efile", formData.get("file"));
+      await axios.post("/upload-equipment", formData, {
+        "Content-Type": "multipart/form-data",
+      });
     } catch (e) {
       console.log(e);
       alert("에러가 일어났습니다.");
@@ -26,6 +38,12 @@ const SetEquip = () => {
       name: "img",
       type: "text",
       register: { required: "입력해 주세요" },
+    },
+    {
+      label: "기구 이미지 파일",
+      name: "file",
+      type: "file",
+      register: { required: "등록해 주세요" },
     },
     {
       type: "submit",
