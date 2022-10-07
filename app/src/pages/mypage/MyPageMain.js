46f2ff0a3e4d5df60b34ad2cb52e731990ab018c
김세희 <82367039+sadie100@users.noreply.github.com>
2022-10-08 01:52:33 +0900

    feat : 내 예약 연결

--- app/src/pages/mypage/MyPageMain.js
+++ app/src/pages/mypage/MyPageMain.js
@@ -1,33 +1,85 @@
 //마이페이지
 
-import React, { useState, useContext } from "react";
-import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
 import Button from "components/common/Button";
 import { useNavigate } from "react-router-dom";
+import useAxiosInterceptor from "hooks/useAxiosInterceptor";
+import { useAuth } from "hooks/useAuthContext";
 
 const MyPageMain = () => {
   const navigate = useNavigate();
-  const handleCancel = () => {
-    if (!window.confirm("예약을 취소하시겠습니까?")) return;
+  const axios = useAxiosInterceptor();
+  const [reservation, setReservation] = useState([]);
+  const { user } = useAuth();
+
+  const getReserve = async () => {
+    try {
+      const { data } = await axios.get("/my-reserve");
+      setReservation(data);
+    } catch (e) {
+      console.log(e);
+      alert("예약 조회 과정에서 에러가 발생했습니다.");
+    }
   };
+
+  const handleCancel = async (reservationId) => {
+    if (!window.confirm("예약을 취소하시겠습니까?")) return;
+    try {
+      const centerId = user.center;
+      await axios.delete(`/center/${centerId}/reserve/${reservationId}`);
+      alert("예약이 취소되었습니다.");
+      await getReserve();
+    } catch (e) {
+      console.log(e);
+      alert("예약 취소 과정에서 에러가 발생했습니다.");
+    }
+  };
+
+  const handleReserve = () => {
+    navigate("/reserve");
+  };
+
+  useEffect(() => {
+    getReserve();
+  }, []);
 
   return (
     <Background>
       <Wrapper>
         <div className="title">마이페이지</div>
         <SubTitle>내 예약</SubTitle>
-        <LongButton>
-          <div>
-            <BoxText>런닝머신1</BoxText>
-            <BoxText className="bold">21:00~21:50</BoxText>
-          </div>
-          <div>
-            <Button backgroundColor="lightgray" onClick={handleCancel}>
-              예약취소
-            </Button>
-          </div>
-        </LongButton>
+        {reservation.length > 0 ? (
+          reservation.map(({ equipName, start, end, reservationId }, idx) => {
+            return (
+              <LongButton key={idx}>
+                <div>
+                  <BoxText>{equipName}</BoxText>
+                  <BoxText className="bold">
+                    {start}~{end}
+                  </BoxText>
+                </div>
+                <div>
+                  <Button
+                    backgroundColor="lightgray"
+                    onClick={async () => await handleCancel(reservationId)}
+                  >
+                    예약취소
+                  </Button>
+                </div>
+              </LongButton>
+            );
+          })
+        ) : (
+          <LongButton>
+            <div>
+              <BoxText>예약 없음</BoxText>
+            </div>
+            <div>
+              <Button onClick={handleReserve}>예약하러 가기</Button>
+            </div>
+          </LongButton>
+        )}
         <SubTitle>계정관리</SubTitle>
         <LongButton className="normal" onClick={() => navigate("/mypage/info")}>
           <BoxText>내 정보 관리</BoxText>
