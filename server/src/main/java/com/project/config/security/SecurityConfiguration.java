be3198c7e2077fe6eae99d272e70180459aa8447
doongidoong <90018305+doongidoong@users.noreply.github.com>
2022-11-08 20:40:09 +0900

    Fix: 파일업로드 수정

--- server/src/main/java/com/project/config/security/SecurityConfiguration.java
+++ server/src/main/java/com/project/config/security/SecurityConfiguration.java
@@ -31,9 +31,9 @@ public class SecurityConfiguration {
                 .and()
 //                .antMatchers("/admin").hasRole("ADMIN")
                 .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-                .antMatchers("/signin", "/signin/**", "/signup","/signup/**",
-                        "/social/**","/centers",
-                        "/upload-file/").permitAll() // 가입 및 인증 주소는 누구나 접근가능
+                .antMatchers("/signin", "/signin/**", "/signup","/signup-center","/signup/**",
+                        "/social/**","/centers","/signout","/equipment/**","/**",
+                        "upload-center","/upload-equipment","/equipment").permitAll() // 가입 및 인증 주소는 누구나 접근가능
                 .antMatchers(HttpMethod.GET, "/helloworld/**","/signup/**").permitAll() // hellowworld로 시작하는 GET요청 리소스는 누구나 접근가능
                 .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
                 .and()
